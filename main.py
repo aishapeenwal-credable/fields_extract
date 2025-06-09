8a23e6ddcd1fe7647b45dcc2a5f39b68475c8295
aishapeenwal-credable <aisha.peenwal@credable.in>
2025-06-09 13:29:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ app.add_middleware(
         "https://c7ef1364-e7e3-4010-929a-ace0b3c13062.lovable.app",
         "https://c7ef1364-e7e3-4010-929a-ace0b3c13062.lovableproject.com",
         "https://preview--agreement-generation.lovable.app",
-        "https://agreement-generation.lovable.app"
+        "https://agreement-generation.lovable.app",
+        "https://id-preview--c7ef1364-e7e3-4010-929a-ace0b3c13062.lovable.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
